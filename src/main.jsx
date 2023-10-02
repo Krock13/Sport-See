65a44ab3a5f53df46d7493da087473ca9456aec6
krock13 <krock.airdrop@hotmail.com>
2023-10-02 13:30:34 +0200

    Modification of the structure to place Header and Sidebar components outside Routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import { Header } from './components/Header/header';
+import { Sidebar } from './components/Sidebar/sidebar';
 import { Home } from './pages/Home/home';
 
 import './utils/style/globalStyle.css';
@@ -10,6 +12,8 @@ import './utils/style/globalStyle.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
+      <Header />
+      <Sidebar />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,11 +1,3 @@
-import { Header } from '../../components/Header/header';
-import { Sidebar } from '../../components/Sidebar/sidebar';
-
 export function Home() {
-  return (
-    <>
-      <Header />
-      <Sidebar />
-    </>
-  );
+  return <></>;
 }
