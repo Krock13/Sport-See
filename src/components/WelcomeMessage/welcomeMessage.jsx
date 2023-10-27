a64713bcc2d238e3e2228adafd2f2e8916b3217b
krock13 <krock.airdrop@hotmail.com>
2023-10-27 14:57:06 +0200

    Implement dynamic first name retrieval in WelcomeMessage component

--- src/components/WelcomeMessage/welcomeMessage.jsx
+++ src/components/WelcomeMessage/welcomeMessage.jsx
@@ -2,18 +2,38 @@
  * WelcomeMessage component that greets the user and shows a congratulatory message.
  * @returns {JSX.Element} The rendered JSX element.
  */
+
+// External dependencies
+import { useFetch } from '../../utils/hooks/fetch';
+
+// Mock data
+import { getUser } from '../../../mocks/user.js';
+
+// Styles
 import styles from './welcomeMessage.module.css';
 
 export function WelcomeMessage() {
+  // Fetch user data
+  const { data, loadingAndErrorComponent } = useFetch(getUser, 1);
+
+  // Retrieve first name from the data, default to 'Utilisateur' if not available
+  const firstName = data?.userInfos?.firstName || 'Utilisateur';
+
   return (
     <div className={styles.welcomeMessageCard}>
-      {/* Greeting the user by name */}
-      <h1>
-        Bonjour <span>Thomas</span>
-      </h1>
+      {loadingAndErrorComponent}
 
-      {/* Congratulatory message for achieving goals */}
-      <p>Félicitation! Vous avez explosé vos objectifs hier 👏</p>
+      {data && Object.keys(data).length > 0 && (
+        <>
+          {/* Greeting the user by name */}
+          <h1>
+            Bonjour <span>{firstName}</span>
+          </h1>
+
+          {/* Congratulatory message for achieving goals */}
+          <p>Félicitation! Vous avez explosé vos objectifs hier 👏</p>
+        </>
+      )}
     </div>
   );
 }
